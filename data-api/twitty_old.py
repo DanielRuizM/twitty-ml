535525e8f512a666e3f66440e6b3ed6941d018f1
danielruizm <danielrumayo@hotmail.com>
2017-08-22 10:18:19 +0200

    better coding

--- data-api/twitty_old.py
+++ data-api/twitty_old.py
@@ -7,13 +7,13 @@ import psycopg2
 import time
 import datetime
 
-
 if len(sys.argv) !=3:
     print("""
     You are introducing wrong parameters, you need:
         --  database_username database_password
     """)
     exit();
+
 
 user_bbdd=sys.argv[1]
 pass_bbdd=sys.argv[2]
@@ -32,28 +32,18 @@ hoy =  datetime.datetime.fromtimestamp(ts).utcnow()
 stt = st[:8]
 print("ejecucion: "+str(hoy))
 
+consumer = oauth.Consumer(key=cred.consumer_key, secret=cred.consumer_secret)
+access_tokens = oauth.Token(key=cred.access_token, secret=cred.access_token_secret)
+client = oauth.Client(consumer, access_tokens)
 
-class TwitterSession(object):
-    def __init__(self,consumer_key,consumer_secret,access_token,access_token_secret):
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
 
-    def add(self):
-        self.x.append(1)
-
-    def login(self):
-        consumer = oauth.Consumer(key=self.consumer_key, secret=self.consumer_secret)
-        access_tokens = oauth.Token(key=self.access_token, secret=self.access_token_secret)
-        client = oauth.Client(consumer, access_tokens)
-        return client
-
-def extracting_twit(data):
-    tweets = json.loads(data)
-    inserts="insert into twits values "
-    tweet_statuses_array=tweets['statuses']
-    for tweet_dic in tweet_statuses_array:
+NorthKorea = "https://api.twitter.com/1.1/search/tweets.json?q=%23NorthKorea&src=tyah"
+response, data = client.request(NorthKorea)
+twits=requests.get('https://api.twitter.com/1.1/search/tweets.json?q=%23NorthKorea&src=tyah')
+inserts="insert into twits values "
+tweets = json.loads(data)
+tweet_statuses_array=tweets['statuses']
+for tweet_dic in tweet_statuses_array:
         created_at=tweet_dic['created_at']
         if type(created_at)== 'NoneType':
             created_at='null'
@@ -71,7 +61,9 @@ def extracting_twit(data):
             hashtags_list.append(hashtag)
         if type(hashtags_list) =='NoneType':
             hashtags_list='null'
+
         user_dic=tweet_dic['user']
+
         user_id=user_dic['id']
         if type(user_id)== 'NoneType':
             user_id='null'
@@ -93,44 +85,19 @@ def extracting_twit(data):
         favourites_count=user_dic['favourites_count'] 
         if type(favourites_count) == 'NoneType':
             favourites_count='null'
+        #print("{},\n{},\n{},\n{},\n{},\n{},\n{},\n{},\n{},\n{},\n{}".format(str(created_at),str(id),str(twit),str(hashtags_list),str(user_id),str(user_name),str(user_location),str(user_screen_name),str(followers_count),str(friends_count),str(favourites_count)))
         insert="('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}'),".format(str(created_at).replace("'",""),str(id).replace("'",""),str(twit).replace("'",""),str(hashtags_list).replace("'",""),str(user_id).replace("'",""),str(user_name).replace("'",""),str(user_location).replace("'",""),str(user_screen_name).replace("'",""),str(followers_count).replace("'",""),str(friends_count).replace("'",""),str(favourites_count).replace("'",""))
         inserts=inserts+insert
-    inserts=inserts[:-1]+';'
-    return inserts
-
-
-def executing_postgres(user_bbdd,pass_bbdd,inserts):
-    try:
-        conn = psycopg2.connect("dbname=twitty user="+user_bbdd+" host=localhost password="+pass_bbdd)
-        cur = conn.cursor()
-        cur.execute(inserts)
-        conn.commit()
-    except psycopg2.Error as e:
-        print(str(e))
-        print('Fallo en la conexion a postgres')
-        sys.exit(1) 
-
-
-
-def main():
-
-    twity=TwitterSession(cred.consumer_key,cred.consumer_secret,cred.access_token,cred.access_token_secret)
-    ouath_client=twity.login()
-
-    NorthKorea = "https://api.twitter.com/1.1/search/tweets.json?q=%23NorthKorea&src=tyah"
-    response, data = ouath_client.request(NorthKorea)
-
-    insert=extracting_twit(data)
-    executing_postgres(user_bbdd,pass_bbdd,insert)
-    
-           
-
-
-
-if __name__ == "__main__":
-    main()
-
-
+inserts=inserts[:-1]+';'
+try:
+    conn = psycopg2.connect("dbname=twitty user="+user_bbdd+" host=localhost password="+pass_bbdd)
+    cur = conn.cursor()
+    cur.execute(inserts)
+    conn.commit()
+except psycopg2.Error as e:
+    print(str(e))
+    print('Fallo en la conexion a postgres')
+    sys.exit(1)
 
 
 
